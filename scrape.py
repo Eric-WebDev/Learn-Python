0495b272c1ff78eb1b4d11aa86a8a8644a6d2314
arek512 <A_lesica@outlook.com>
2020-10-02 17:10:56 +0100

    add practise

--- scrape.py
+++ scrape.py
@@ -15,19 +15,22 @@ subtext2 = soup2.select('.subtext')
 mega_links = links + links2
 mega_subtext = subtext + subtext2
 
+
 def sort_stories_by_votes(hnlist):
-  return sorted(hnlist, key= lambda k:k['votes'], reverse=True)
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
+
 
 def create_custom_hn(links, subtext):
-  hn = []
-  for idx, item in enumerate(links):
-    title = item.getText()
-    href = item.get('href', None)
-    vote = subtext[idx].select('.score')
-    if len(vote):
-      points = int(vote[0].getText().replace(' points', ''))
-      if points > 99:
-        hn.append({'title': title, 'link': href, 'votes': points})
-  return sort_stories_by_votes(hn)
- 
+    hn = []
+    for idx, item in enumerate(links):
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
+    return sort_stories_by_votes(hn)
+
+
 pprint.pprint(create_custom_hn(mega_links, mega_subtext))
